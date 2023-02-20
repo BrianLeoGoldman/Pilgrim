686f7a501d77eefa66cde08427050872cb55af09
Brian Goldman <leonel89011@gmail.com>
2023-02-20 19:49:10 -0300

    Camera movement following player implemented

--- level.py
+++ level.py
@@ -25,7 +25,7 @@ class Level:
 
     def run(self):
         # update and draw the game
-        self.visible_sprites.custom_draw()
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         debug(self.player.direction)
 
@@ -36,7 +36,14 @@ class YSortCameraGroup(pygame.sprite.Group):
         # general setup
         super().__init__()
         self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2  # half of the screen width
+        self.half_height = self.display_surface.get_size()[1] // 2  # half of the screen height
+        self.offset = pygame.math.Vector2()  # starts as (0,0)
 
-    def custom_draw(self):
+    def custom_draw(self, player):
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
         for sprite in self.sprites():
-            self.display_surface.blit(sprite.image, sprite.rect)
+            offset_position = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_position)
