86efbcd47ba64af1623ef52057cfd23fbdfe1e99
Brian Goldman <leonel89011@gmail.com>
2023-03-01 16:44:06 -0300

    Grass and enemies can be destroyed

--- level.py
+++ level.py
@@ -23,6 +23,8 @@ class Level:
 
         # attack sprites
         self.current_attack = None
+        self.attack_sprites = pygame.sprite.Group()
+        self.attackable_sprites = pygame.sprite.Group()
 
         # sprite setup
         self.create_map()
@@ -52,7 +54,11 @@ class Level:
                         if style == 'grass':
                             #  create a grass tile
                             random_grass_image = choice(graphics['grass'])
-                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'grass', random_grass_image)
+                            Tile(
+                                (x, y),
+                                [self.visible_sprites, self.obstacle_sprites, self.attackable_sprites],
+                                'grass',
+                                random_grass_image)
                             # TODO: remove self.obstacle_sprites so the grass does not have collision
                         if style == 'object':
                             #  create an object tile
@@ -78,10 +84,14 @@ class Level:
                                     monster_name = 'raccoon'
                                 else:
                                     monster_name = 'squid'
-                                Enemy(monster_name, (x, y), [self.visible_sprites], self.obstacle_sprites)
+                                Enemy(
+                                    monster_name,
+                                    (x, y),
+                                    [self.visible_sprites, self.attackable_sprites],
+                                    self.obstacle_sprites)
 
     def create_attack(self):
-        self.current_attack = Weapon(self.player, [self.visible_sprites])
+        self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
 
     def create_magic(self, style, strength, cost):
         print(style)
@@ -93,6 +103,14 @@ class Level:
             self.current_attack.kill()
         self.current_attack = None
 
+    def player_attack_logic(self):
+        if self.attack_sprites:
+            for attack_sprite in self.attack_sprites:
+                collision_sprites = pygame.sprite.spritecollide(attack_sprite, self.attackable_sprites, True)
+                if collision_sprites:
+                    for target_sprite in collision_sprites:
+                        target_sprite.kill()
+
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
@@ -100,7 +118,7 @@ class Level:
         self.visible_sprites.enemy_update(self.player)
         # debug(self.player.direction)
         # debug(self.player.status)
-
+        self.player_attack_logic()
         self.ui.display(self.player)
 
 
