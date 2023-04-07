5980ecefe6ea0b7a01759258d470ed851ed9fabe
Brian Goldman <leonel89011@gmail.com>
2023-04-07 01:36:17 -0300

    Sound implemented

--- player.py
+++ player.py
@@ -52,6 +52,10 @@ class Player(Entity):
         self.hurt_time = None
         self.invulnerability_duration = 500
 
+        # import a sound
+        self.weapon_attack_sound = pygame.mixer.Sound('./audio/sword.wav')
+        self.weapon_attack_sound.set_volume(0.2)
+
     def import_player_assets(self):
         character_path = './graphics/player/'
         self.animations = {
@@ -91,6 +95,7 @@ class Player(Entity):
                 self.attacking = True
                 self.attack_time = pygame.time.get_ticks()
                 self.create_attack()
+                self.weapon_attack_sound.play()  # TODO: should the sound be inside create_attack()?
 
             # magic input
             if keys[pygame.K_LCTRL]:
